1aa56b39b4fb739806005ea060e504459d7dc793
JnckCode <54052722+jnckcode@users.noreply.github.com>
2021-10-23 17:20:48 +0700

    Update Index for reminder
    Hahahah

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 echo "Hello World";
+
+echo "https://api.telegram.org/bot1925075909:AAGMadqElAbeeizgbX-NKblfbznXd3AiGdU/setWebhook?url=https://telebot-production-2d90.up.railway.app/telebot.php";
 ?>
