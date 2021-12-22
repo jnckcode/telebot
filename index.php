4a542be0c79525a83ec733d0ec7241da00a435a3
JnckCode <54052722+jnckcode@users.noreply.github.com>
2021-12-22 22:48:57 +0700

    Update index.php
    69

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-echo "Hello World \n";
+echo "Hello World";
+echo "<br>";
 
 echo "https://api.telegram.org/bot1925075909:AAHJTTWLcflCBJ2nG3-jLQzKf8GErLqvSgY/setWebhook?url=https://telebot-production-2d90.up.railway.app/telebot.php";
 ?>
