650c3489ebecf8fca5a1e450841293e3b6faa87e
JnckCode <54052722+jnckcode@users.noreply.github.com>
2021-12-22 22:44:25 +0700

    Fixxxxxx

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "Hello World";
+echo "Hello World \n";
 
-echo "https://api.telegram.org/bot1925075909:AAGMadqElAbeeizgbX-NKblfbznXd3AiGdU/setWebhook?url=https://telebot-production-2d90.up.railway.app/telebot.php";
+echo "https://api.telegram.org/bot1925075909:AAHJTTWLcflCBJ2nG3-jLQzKf8GErLqvSgY/setWebhook?url=https://telebot-production-2d90.up.railway.app/telebot.php";
 ?>
